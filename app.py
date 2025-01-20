f151fd7525a35579bf6e8d7bafd01ae4a5a2a7ab
rems007 <zoya.saiyed.545@my.werbr.ca>
2025-01-20 13:35:43 -0500

    bunch of changes

--- app.py
+++ app.py
@@ -1,21 +1,97 @@
 from flask import Flask, render_template, redirect, request
-from flask_scss import Scss
-from flask_sqlalchemy import SQLAlchemy
-from unit16_converters import floatToUint16, floatConvertion
-from ModbusTCPClient import Modbus
-from datetime import datetime
+from flask_socketio import SocketIO, emit 
+from flask_mail import Mail, Message 
+import smtplib
+import websocket
+import threading
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+load_dotenv()
 
-
-# My App Setup
 app = Flask(__name__)
-Scss(app)
+socketio = SocketIO(app)
 
-ModbusHost = 'localhost'
-Modbusport = 1562
-unitID = 1
+websocket_data = None
 
-modbusClient = Modbus(ModbusHost,Modbusport,unitID)
-connection = modbusClient.modbusConnect()    
+def on_message(ws, message):
+    global websocket_data
+    websocket_data=message
+    print("Received from websocket: ", message)
+    socketio.emit('new_data', {'data': message})
+
+def on_error(ws, error):
+    print("Error: ", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("Closed websocket connection")
+
+def on_open(ws):
+    print("Websocket connection established")
+
+def start_websocket():
+    ws_url=  "ws://192.168.3.126:1880/ws/temp"
+    print(f"Connecting to websocket at {ws_url}")
+    ws = websocket.WebSocketApp(ws_url,
+                                on_message = on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+def run_websocket_thread():
+    websocket_thread =threading.Thread(target=start_websocket)
+    websocket_thread.daemon=True
+    websocket_thread.start()
+
+def send_email_to_osticket(user_email, subject, message):
+    from_email = os.getenv("FROM_EMAIL")
+    from_password = os.getenv("FROM_PASSWORD")
+
+    to_email = "support@hgvsupport.ddns.net"
+
+    email_message = MIMEMultipart()
+    email_message['From'] = user_email
+    email_message['To'] = to_email
+    email_message['Subject'] = subject
+    email_message.attach(MIMEText(message, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.set_debuglevel(1)
+
+
+        server.starttls()
+        server.login(from_email, from_password)
+
+        text = email_message.as_string()
+        result = server.sendmail(from_email, to_email, text)
+
+        if not result:
+            print("Email sent to osTicket!!")
+        else:
+            print(f"Failed to send email to: {result}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server.quit()
+
+
+@app.route('/ssa/temperature-adjustment')
+def temperature_adjustment():
+    global websocket_data
+    roomTemp= websocket_data if websocket_data else 22
+    SetTemp = roomTemp
+
+    return render_template("temp.html", roomTemp=roomTemp, SetTemp=SetTemp)
+
+@socketio.on('request_data') 
+def handle_request_data():
+    global websocket_data
+    if websocket_data:
+        emit('new_data', {'data': websocket_data})
 
 #Home Page
 @app.route('/')
@@ -26,7 +102,7 @@ def home():
 
 
 @app.route('/activities')
-def activties():
+def activities():
     return render_template('activities.html')
 
 @app.route('/sales')
@@ -49,7 +125,6 @@ def rotary():
 def report():
     return render_template('reportprob.html')
 
-
 #Everything In the Ssa directory
 @app.route('/ssa')
 def ssa():
@@ -63,51 +138,25 @@ def weather():
 def header():
     return render_template('header.html')
 
-@app.route('/ssa/temperature-adjustment',methods=['POST', 'GET'])
-def temperature_adjustment():
-    IR = modbusClient.modbusRead('ir',0,6)
-    HR = modbusClient.modbusRead('hr',0,4)
-    SetTemp = floatConvertion(HR[0], HR[1])
-    SetHimid = floatConvertion(HR[2], HR[3]) 
-    roomTemp = floatConvertion(IR[0], IR[1])
-    roomHimid = floatConvertion(IR[4], IR[5])
-    return render_template('temp.html',roomTemp=roomTemp, roomHimid=roomHimid, SetTemp=SetTemp, SetHimid=SetHimid)
-
-@app.route('/submit',methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        InputTemp = request.form['InputTemp']
-        InputHimid = request.form['InputHimid']
-        
-        ST_Unit16 = floatToUint16(float(InputTemp))
-        SH_Uint16 = floatToUint16(float(InputHimid))
-        modbusClient.modbusWrite('hr',0,ST_Unit16, True)
-        modbusClient.modbusWrite('hr',2,SH_Uint16, True)
-        return redirect('/temperature-adjustment')
-
 @app.route('/send-email', methods=['GET', 'POST'])
 def send_email():
-    print(f"Request Method: {request.method}") #Not Needed? Debug ig
     if request.method == 'POST':
+        try:
+            user_email = request.form['email']
         # Get subject and message from the form
-        subject = request.form['subject']
-        message = request.form['message']
+            subject = request.form['subject']
+            message = request.form['message']
        
-        # Hard-code the recipient's email
-        recipient = 'zsaiyed@gmail.com'  # Replace with the desired recipient email
+            send_email_to_osticket(user_email, subject, message)
+            return render_template('reportprob.html', message="Email sent!!")
+        
 
-        # Create the mailto link with the subject and body
-        mailto_link = f"mailto:{recipient}?subject={subject}&body={message}"
-
-        # Redirect the user to their email client (Gmail in this case)
-        return redirect(mailto_link)
-
+        except Exception as e:
+            return render_template('reportprob.html', message=f"Error sending email: {str(e)}")
     return render_template('reportprob.html')
 
 
-
-
-
-# Runner and Dubgger
+# Runner and Dubugger
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    run_websocket_thread()
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
---
+++ backupModbus.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from unit16_converters import floatToUint16, floatConvertion
+from ModbusTCPClient import Modbus
+from datetime import datetime
+
+
+# My App Setup
+app = Flask(__name__)
+Scss(app)
+
+ModbusHost = 'localhost'
+Modbusport = 1562
+unitID = 1
+
+modbusClient = Modbus(ModbusHost,Modbusport,unitID)
+connection = modbusClient.modbusConnect()    
+
+#Home Page
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+#items go in order of the Homepage
+
+
+@app.route('/activities')
+def activties():
+    return render_template('activities.html')
+
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
+
+@app.route('/location')
+def location():
+    return render_template('location.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contactInfo.html')
+
+@app.route('/rotary')
+def rotary():
+    return render_template('rotary.html')
+    
+@app.route('/report')
+def report():
+    return render_template('reportprob.html')
+
+
+#Everything In the Ssa directory
+@app.route('/ssa')
+def ssa():
+    return render_template('SSA.html')
+
+@app.route('/ssa/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+@app.route('/ssa/temperature-adjustment',methods=['POST', 'GET'])
+def temperature_adjustment():
+    IR = modbusClient.modbusRead('ir',0,6)
+    HR = modbusClient.modbusRead('hr',0,4)
+    SetTemp = floatConvertion(HR[0], HR[1])
+    SetHimid = floatConvertion(HR[2], HR[3]) 
+    roomTemp = floatConvertion(IR[0], IR[1])
+    roomHimid = floatConvertion(IR[4], IR[5])
+    return render_template('temp.html',roomTemp=roomTemp, roomHimid=roomHimid, SetTemp=SetTemp, SetHimid=SetHimid)
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        InputTemp = request.form['InputTemp']
+        InputHimid = request.form['InputHimid']
+        
+        ST_Unit16 = floatToUint16(float(InputTemp))
+        SH_Uint16 = floatToUint16(float(InputHimid))
+        modbusClient.modbusWrite('hr',0,ST_Unit16, True)
+        modbusClient.modbusWrite('hr',2,SH_Uint16, True)
+        return redirect('/temperature-adjustment')
+
+@app.route('/send-email', methods=['GET', 'POST'])
+def send_email():
+    print(f"Request Method: {request.method}") #Not Needed? Debug ig
+    if request.method == 'POST':
+        # Get subject and message from the form
+        subject = request.form['subject']
+        message = request.form['message']
+       
+        # Hard-code the recipient's email
+        recipient = 'zsaiyed@gmail.com'  # Replace with the desired recipient email
+
+        # Create the mailto link with the subject and body
+        mailto_link = f"mailto:{recipient}?subject={subject}&body={message}"
+
+        # Redirect the user to their email client (Gmail in this case)
+        return redirect(mailto_link)
+
+    return render_template('reportprob.html')
+
+
+
+
+
+# Runner and Dubgger
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
